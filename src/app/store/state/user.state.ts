2f99106395c7c964b2442b67ff927f3a7a690e7b
Hélène Baudrand <baudrand.helene@gmail.com>
2021-07-14 17:35:03 +0200

    initialised state

---
+++ src/app/store/state/user.state.ts
@@ -0,0 +1,11 @@
+import { IUser } from '../../models/user.interface'
+
+export interface IUserState {
+  users: IUser[]
+  selectedUser: IUser
+}
+
+export const initialUserState: IUserState = {
+  users: [],
+  selectedUser: null
+}
